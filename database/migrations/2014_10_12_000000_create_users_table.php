9cd3ee1294d65eafeccbbd67cff322b4f8666178
alvin.pino <alvin.pino@procuraduria.gob.pa>
2024-01-25 14:04:31 -0500

    Nuevos Cambios-campos nuevos

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('apellido');
+            $table->string('usuario');
+            $table->integer('fk_despacho');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
